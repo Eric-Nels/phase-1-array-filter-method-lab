afbf24a592fd0f571384821c38440a88cef16d75
Eric Nelson <ernelson12o0@gmail.com>
2023-03-07 15:20:57 -0500

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(item => item.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(item => item[0, 1] === string[0, 1])
+}
+
+function matchName(array, string) {
+    return array.filter(object => object.name === string)
+}
